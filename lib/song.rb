2f447888d619d6912af269d7d226db858f3141ef
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-29 19:41:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,17 +30,27 @@ class Song
 
   def self.genre_count
   genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
+  @@genres.each do |i|
+    if genre_count[i]
+      genre_count[i] += 1
     else
-      genre_count[genre] = 1
+      genre_count[i] = 1
     end
   end
   genre_count
 end
 
-  def self.artist_coutn
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |i|
+      if artist_count[i]
+        artist_count[i] += 1
+      else
+        artist_count[i] = 1
+      end
+    end
+    artist_count
+        
 
   end
 
