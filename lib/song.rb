100b3daaf5a4864c7b245da6129efa2898813c1e
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-29 19:28:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def self.genre_count
+    @@genre_count = Hash[genre, genre.length]
+  end
+end
